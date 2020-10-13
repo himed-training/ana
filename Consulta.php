8f005373fd17c59f8f9d1f67399851e589b4f812
Ana Vergara <ana.vergara@himedsolutions.com>
2020-10-13 15:47:02 -0500

    first commit

---
+++ Consulta.php
@@ -0,0 +1,24 @@
+<?php
+final class Consulta{
+
+    private $paciente;
+    private $fecha;
+    private $ms;//Mesajes personalizados
+    public function __construct($fecha,$paciente){
+                
+    }
+     public function getPaciente(){
+        return $this->paciente;
+    }
+    public function getFecha(){
+        return $this->fecha;
+    }
+    public isPaciente($paciente){
+        
+    }
+    public isFecha($fecha){
+
+    }
+        
+    
+}
